7f21614abc7690b9ce8727ea2a5c609b92a2add8
avsuryateja991 <avsuryateja991@gmail.com>
2025-06-29 22:50:42 +0530

    added new varaible file

---
+++ 03-varaibles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1 = surya
+PERSON2 = Teja
+echo "$PERSON1 : Hi $PERSON2"
+echo "$PERSON2 : Hi $PERSON1, I am good.. How are you"
+echo "$PERSON1 : I am good $PERSON2, What's up"
+echo "$PERSON2 : Sky"
