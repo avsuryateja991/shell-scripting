37cb45a5aaa4de62e56de74873c015d7b90f6499
avsuryateja991 <avsuryateja991@gmail.com>
2025-06-29 23:00:08 +0530

    added attributes

---
+++ 04-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Enter user name:"
+read -s $PERSON1
+echo "User name is: $PERSON1"
+
+echo "Enter pass:"
+read -s $PERSON2
+echo "Pass entered is: $PERSON2"
+
+
+echo "$PERSON1 : Hi $PERSON2"
+echo "$PERSON2 : Hi $PERSON1, I am good.. How are you"
+echo "$PERSON1 : I am good $PERSON2, What's up"
+echo "$PERSON2 : Sky"
